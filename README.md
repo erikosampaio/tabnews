235d47409094bd4239dedb28cbcab3f39d6ebbe2
Ériko Sampaio <90206982+erikosampaio@users.noreply.github.com>
2024-06-15 21:11:37 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tabnews
+
 Projeto iniciado para aprendizado com as tecnologias JS (Node.js, React e Next.js)
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Eu amo minha família e sou o pai e marido mais feliz do mundo. Amo vocês!</h1>
+  return (
+    <h1>
+      Eu amo minha família e sou o pai e marido mais feliz do mundo. Amo vocês!
+    </h1>
+  );
 }
 
 export default Home;
